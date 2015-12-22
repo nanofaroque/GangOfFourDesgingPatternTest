f10ad1ebda942f6290580c685f33091b42776d4c
Md Faroque <faroque.ae@gmail.com>
2015-12-21 18:49:05 -0600

    description add in factory design pattern

--- src/factorypattern/Testing.java
+++ src/factorypattern/Testing.java
@@ -6,6 +6,13 @@ public class Testing {
 		AnimalFactory factory=new AnimalFactory();
 		Animal animal=factory.createAnimal("cat");
 		animal.eat();
+		/*
+		 * Cat object has been created here but if you have any extra method other than eat
+		 * you can not call it because: You can only access the methods defined on the interface through 
+		 * a variable declared as having that interface, 
+		 * even if the concrete class has additional methods
+		 * */
+		
 	}
 
 }
