5498780b90fdb88fc7abf3d9672e38ab2c7b40d4
Md Faroque <faroque.ae@gmail.com>
2015-12-24 16:58:33 -0600

    Sample code

---
+++ src/template_pattern/Cricket.java
@@ -0,0 +1,22 @@
+package template_pattern;
+
+public class Cricket extends Game {
+
+	@Override
+	public void init() {
+		System.out.println("Cricket init");
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Cricket start");
+
+	}
+
+	@Override
+	public void end() {
+		System.out.println("Cricket end");
+
+	}
+
+}
---
+++ src/template_pattern/Football.java
@@ -0,0 +1,23 @@
+package template_pattern;
+
+public class Football extends Game {
+
+	@Override
+	public void init() {
+		System.out.println("Football init");
+
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Football start");
+
+	}
+
+	@Override
+	public void end() {
+		System.out.println("Football end");
+
+	}
+
+}
