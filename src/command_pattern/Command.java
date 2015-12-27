36235a1a8baaf4927719b65923ffe72345afbd7e
Md Faroque <faroque.ae@gmail.com>
2015-12-26 23:32:56 -0600

    no message

--- src/command_pattern/Command.java
+++ src/command_pattern/Command.java
@@ -3,6 +3,7 @@ package command_pattern;
 //this will be used as a command 
 public interface Command {
 	//all of the concrete command will execute this method 
+	//Single method interfaces are called functional interface 
 	void execute();
 
 }
